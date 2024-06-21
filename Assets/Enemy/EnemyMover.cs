2bc5d15899198a130971780115bc81d12119027f
anurupkoli <anurupkoli01@gmail.com>
2024-06-21 22:22:48 +0530

    Adding automatic path detection system

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -1,26 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
 {
-    [SerializeField] List<WayPoint> wayPoints = new List<WayPoint>();
-    [SerializeField] [Range(0f, 5f)] float speed = 1f;
-    private void Start() {
-        StartCoroutine(moveEnemy());
+    [SerializeField][Range(0f, 5f)] float speed = 1f;
+    List<WayPoint> wayPoints = new List<WayPoint>();
+    GameObject pathTiles;
+    private void Start()
+    {
+        pathTiles = GameObject.FindGameObjectWithTag("PathTiles");
+        FindPaths();
+        StartCoroutine(MoveEnemy());
     }
 
-    IEnumerator moveEnemy(){
-        foreach(WayPoint wayPoint in wayPoints){
+    void FindPaths()
+    {
+        List<GameObject> paths = GameObject.FindGameObjectsWithTag("Path").ToList();
+        paths = paths.OrderBy(path => path.name).ToList();
+        AddToWayPoints(paths);
+    }
+
+    void AddToWayPoints(List<GameObject> paths){
+        wayPoints.Clear();
+        foreach (GameObject path in paths)
+        {
+            wayPoints.Add(path.GetComponent<WayPoint>());
+            AddToPathTiles(path);
+        }
+    }
+
+    void AddToPathTiles(GameObject path){
+        path.transform.parent = pathTiles.transform;
+    }
+
+    IEnumerator MoveEnemy()
+    {
+        foreach (WayPoint wayPoint in wayPoints)
+        {
             Vector3 startPosition = transform.position;
             Vector3 endPosition = new Vector3(wayPoint.X, transform.position.y, wayPoint.Z);
-            transform.LookAt(endPosition);
+
+            HandleEnemyFacing(endPosition);
+            
             float distance = 0f;
-            while(distance < 1){
+            while (distance < 1)
+            {
                 distance += Time.deltaTime * speed;
                 transform.position = Vector3.Lerp(startPosition, endPosition, distance);
                 yield return new WaitForEndOfFrame();
             }
         }
+    }
+
+    void HandleEnemyFacing(Vector3 endPosition){
+         transform.LookAt(endPosition);
     }
 }
