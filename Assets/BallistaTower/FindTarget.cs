6acae53f25696dff7c05b6761b4c68537c969073
anurupkoli <anurupkoli01@gmail.com>
2024-06-22 00:45:36 +0530

    Refining Ballista Attacking System

--- Assets/BallistaTower/FindTarget.cs
+++ Assets/BallistaTower/FindTarget.cs
@@ -1,25 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FindTarget : MonoBehaviour
 {
     [SerializeField] Transform weapon;
-    [SerializeField] Transform enemy;
-    // Start is called before the first frame update
-    void Start()
-    {
-        FindEnemy();
-    }
-
-    // Update is called once per frame
+    [SerializeField] ParticleSystem boultParticles;
+    [SerializeField] float range = 15f;
+    Transform target;
+    
     void Update()
     {
-        FindEnemy();
-        transform.LookAt(enemy);
+        FindClosestTarget();
+        LookAtTarget();
     }
 
-    void FindEnemy(){
-        enemy = GameObject.FindGameObjectWithTag("Enemy").transform;
+    void FindClosestTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+        foreach (GameObject enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (targetDistance < maxDistance)
+            {
+                closestTarget = enemy;
+                maxDistance = targetDistance;
+            }
+        }
+        target = closestTarget.transform;
+    }
+
+    void LookAtTarget()
+    {
+        float distance = Vector3.Distance(transform.position, target.transform.position);
+        transform.GetChild(1).LookAt(target);
+        if (distance <= range)
+        {
+            Attack(true);
+        }
+        else{
+            Attack(false);
+        }
+    }
+
+    void Attack(bool isAttacking){
+        var emissionModule = boultParticles.emission;
+        emissionModule.enabled = isAttacking;
     }
 }
