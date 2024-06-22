1b4c69ba474856acd20ab1cf51ca222050a66e02
anurupkoli <anurupkoli01@gmail.com>
2024-06-22 17:08:18 +0530

    Adding UI

---
+++ Assets/UI/UpdateGold.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpdateGold : MonoBehaviour
+{
+    Bank bank;
+    TMP_Text textMeshPro;
+    void Start(){
+        bank = FindAnyObjectByType<Bank>();
+        textMeshPro = GetComponent<TMP_Text>();
+    }
+
+    void Update(){
+        UpdateText();
+    }
+
+    void UpdateText(){
+        textMeshPro.text = "Gold : " +  bank.CurrentMoney.ToString();
+    }
+}
